9576977d43b88231296218dd8d0b2d9f956bbff0
ReinaldyZA <115777212+ReinaldyZA@users.noreply.github.com>
2026-05-30 01:53:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -285,7 +285,7 @@ def inject_css():
         border: 1px solid #E5E7EB !important;
         background-color: #FFFFFF !important;
         box-shadow: 0 1px 3px rgba(15, 23, 42, 0.04);
-        padding: 60px 62px !important;
+        padding: 20px 22px !important;
         transition: all 0.25s ease;
     }
     [data-testid="stVerticalBlockBorderWrapper"]:hover {
@@ -583,8 +583,8 @@ def inject_css():
     /* Wrapper card untuk Komposisi Polutan & Hasil Prediksi */
     .sim-card {
         background: #FFFFFF;
-        border-radius: 90px;
-        padding: 60px 70px;
+        border-radius: 20px;
+        padding: 26px 28px;
         border: 1px solid #E5E7EB;
         box-shadow: 0 1px 3px rgba(15, 23, 42, 0.04),
                     0 4px 14px -4px rgba(15, 23, 42, 0.06);
